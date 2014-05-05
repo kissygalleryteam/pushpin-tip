30c0271fd9467591a88dfeb636ff4197033c7f54
圆影 <yuanying.xh@alibaba-inc.com>
2014-05-05 18:29:33 +0800

    first commit js/css

--- 1.0/guide/index.md
+++ 1.0/guide/index.md
@@ -7,11 +7,51 @@ Gallery是。
 * demo：[http://gallery.kissyui.com/gallery/1.0/demo/index.html](http://gallery.kissyui.com/gallery/1.0/demo/index.html)
 
 ## 初始化组件
-		
-    S.use('gallery/gallery/1.0/index', function (S, Gallery) {
-         var gallery = new Gallery();
+        
+#### 只使用核心功能
+
+    S.use('gallery/gallery/1.0/index', function (S, PushpinTip) {
+          new PushpinTip({
+            closable: false,
+            elcls: 'demo',
+            tips: [
+                {
+                    cls: 'top',
+                    node: '#demo5',
+                    text: '上面的提示上面的提示',
+                    offset: [-87,50]
+                }
+            ]
+          });
     })
-	
-	
+        
+#### 使用默认主题
+
+    S.use('gallery/pushpin-tip/1.0/index,gallery/pushpin-tip/1.0/index.css', function (S, PushpinTip) {
+          new PushpinTip({
+            closable: false,
+            elcls: 'demo',
+            tips: [
+                {
+                    cls: 'top',
+                    node: '#demo5',
+                    text: '上面的提示上面的提示',
+                    offset: [-87,50]
+                }
+            ]
+          });
+    })
+    
+    
 
 ## API说明
+
+### 配置属性
+
+`closable` 是否显示关闭按钮
+`elcls` 提示层通用class
+`tips` 提示层数组
+    **cls** 提示层独立class
+    **node** 提示层定位节点
+    **text** 提示文案
+    **offset** 提示层位置偏移
--- 1.0/index.js
+++ 1.0/index.js
@@ -1,29 +1,89 @@
 /**
  * @fileoverview 
  * @author 圆影<yuanying.xh@alibaba-inc.com>
- * @module gallery
+ * @module pushpin-tip
+
+配置示例：
+{
+    closable: true,
+    elcls: 'ks-pushpin',
+    tips: [
+        {
+            cls: 'right',
+            node: null,
+            offset: [0,0],
+            text: ''
+        }
+    ]
+}
+
  **/
-KISSY.add(function (S, Node,Base) {
-    var EMPTY = '';
+KISSY.add(function (S, Node) {
     var $ = Node.all;
-    /**
-     * 
-     * @class Gallery
-     * @constructor
-     * @extends Base
-     */
-    function Gallery(comConfig) {
-        var self = this;
-        //调用父类构造函数
-        Gallery.superclass.constructor.call(self, comConfig);
+    var Config = {
+        closable: true,
+        elcls: 'ks-pushpin',
+        tips: []
     }
-    S.extend(Gallery, Base, /** @lends Gallery.prototype*/{
+    var TPL = '<div class="{elcls} {cls}"><a href="javascript:{}" class="ks-pushpin-close">close</a><div class="ks-pushpin-text">{text}</div></div>';
 
-    }, {ATTRS : /** @lends Gallery*/{
+    function PushpinTip(){
+        this._init.apply(this,arguments);
+    }
 
-    }});
-    return Gallery;
-}, {requires:['node', 'base']});
+    S.augment(PushpinTip,{
+        _init: function(config){
+            this.cfg = S.merge(Config,config||{});
+            if (this.cfg.tips.length>0) {
+                this._createHTML(this.cfg.tips);
+            }
+        },
+        _createHTML: function(tips){
+            var self = this;
+            var fragment = $(document.createDocumentFragment());
+            S.each(tips,function(tip,index){
+                var tip = S.merge({
+                    cls: 'right',
+                    node: null,
+                    offset: [0,0],
+                    text: ''
+                },tip);
+                if (!tip.node) return;
+                tip.node = typeof tip.node == 'string' ? $(tip.node) : tip.node;
+
+                var html = S.substitute(TPL,{
+                    elcls: self.cfg.elcls,
+                    cls: tip.cls,
+                    text: tip.text
+                });
+                var t = tip.node[0].tip = $(html);
+                if (self.cfg.closable) {
+                    self._bind(t);
+                } else{
+                    t.all('a').remove();
+                };
+                self._position(t,tip.node,tip.offset);
+                fragment.append(t);
+            });
+            $('body').append(fragment);
+        },
+        _position: function(tip,node,offset){
+            tip.css({
+                'position': 'absolute',
+                'top': node.offset().top+offset[0],
+                'left': node.offset().left+offset[1]
+            });
+        },
+        _bind: function(tip){
+            tip.all('a').on('click',function(){
+                tip.remove();
+            });
+        }
+    });
+
+    return PushpinTip;
+
+}, {requires:['node']});
 
 
 
