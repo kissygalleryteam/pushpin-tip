e303389f33b0e32bd2d430f4246ac035c59c6e2d
圆影 <yuanying.xh@alibaba-inc.com>
2014-05-09 11:53:40 +0800

    change css

--- 1.0/build/mini-min.js
+++ 1.0/build/mini-min.js
@@ -1,2 +1,2 @@
-/*!build time : 2014-05-06 6:17:03 PM*/
+/*!build time : 2014-05-09 11:53:26 AM*/
 KISSY.add("gallery/pushpin-tip/1.0/mini",function(a,b){function c(){}var d=(b.all,a.Event.Target);return a.augment(c,d,{}),c},{requires:["node","lang"]});
