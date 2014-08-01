1956103a7a680c55d1b046b75277822113953188
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-01 13:44:58 +0800

    sync doc

---
+++ doc/build/mini.js
@@ -0,0 +1,34 @@
+/*
+combined files : 
+
+kg/pushpin-tip/2.0.0/mini
+
+*/
+/**
+ * @fileoverview 
+ * @author 圆影<yuanying.xh@alibaba-inc.com>
+ * @module kg
+ **/
+KISSY.add('kg/pushpin-tip/2.0.0/mini',function (S, Node, Lang) {
+    var $ = Node.all,
+        EventTarget = S.Event.Target;
+    /**
+     *
+     * @class Gallery
+     * @constructor
+     */
+    function Gallery(config) {
+
+    }
+
+    S.augment(Gallery, EventTarget, /** @lends Gallery.prototype*/{
+
+    });
+
+    return Gallery;
+
+}, {requires:['node', 'lang']});
+
+
+
+
